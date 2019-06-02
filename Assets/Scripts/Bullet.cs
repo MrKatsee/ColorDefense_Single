8cb43c7e8a37f7dddcb8a17f6f29e2a6fd75726f
KimHoseong <ghtjd000129@naver.com>
2019-06-02 09:46:30 +0900

    몇가지 스크립트 생성

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -33,4 +33,6 @@ public class Bullet : MonoBehaviour
         StopAllCoroutines();
         Destroy(gameObject);
     }
+
+    
 }
--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -22,4 +22,6 @@ public class PlayManager : MonoBehaviour
     {
         return player.GetComponent<PlayerData>();
     }
+
+    
 }
